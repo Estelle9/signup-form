8f48c8d4858f89a6f556233edf3356e209f178ca
Estelle9 <estelle9.dev@gmail.com>
2020-12-27 18:48:19 +0100

    signup-form

---
+++ main.js
@@ -0,0 +1,38 @@
+const emailInput = document.getElementById("email-input");
+const button = document.getElementById('btn');
+const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+const errorMessage = document.getElementById('error-message');
+
+document.addEventListener('DOMContentLoaded', () => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const input = document.getElementsByTagName('input');
+        const errorMsg = document.getElementsByClassName('error');
+        for (let i = 0; i < input.length; ++i) {
+            if (input[i].value == '') {
+                errorMsg[i].style.display = 'block';
+                errorMsg[i].style.color = 'red';
+                input[i].style.background = 'url(images/icon-error.svg) right 20px center no-repeat';
+
+            } else {
+                errorMsg[i].style.display = 'none';
+                input[i].style.background = '';
+            }
+        }
+
+        if (emailInput.value !== '' && !filter.test(emailInput.value)) {
+            errorMessage.style.display = 'block';
+            errorMessage.style.color = 'red';
+            emailInput.style.background = 'url(images/icon-error.svg) right 20px center no-repeat';
+
+        } else {
+            errorMessage.style.display = 'none';
+            emailInput.style.background = '';
+        }
+    })
+});
+
+
+
+
